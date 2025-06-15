e95316db6efa7115a5d86bc56b0a73635050fbd7
Tattzy25 <tattzy2025@gmail.com>
2025-06-14 21:36:27 -0700

    Add Vercel Analytics integration

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Toaster } from 'react-hot-toast'
+import { Analytics } from '@vercel/analytics/react'
 
 // Error Boundary Component
 class ErrorBoundary extends React.Component {
@@ -116,6 +117,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ErrorBoundary>
       <App />
       <Toaster toastOptions={toastOptions} />
+      <Analytics />
     </ErrorBoundary>
   </React.StrictMode>,
 )
